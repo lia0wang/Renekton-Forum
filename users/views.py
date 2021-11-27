6c0b11a62f58225c82bd2f8b9b7be9f1ede096a0
WANG LIAO <z5306312@ad.unsw.edu.au>
2021-11-27 18:17:42 +1100

    finish user/register

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    """Register a new user"""
+    if request.method != 'POST':
+        # display blank form
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # log the user in 
+            login(request, new_user)
+            # and then redirect to home page.
+            return redirect('app_renekton:index')
+
+    # if the form is blank or invalid
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
